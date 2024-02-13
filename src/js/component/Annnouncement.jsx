223a0f1d5b4c97bde788191b620030aef082df3e
Jorge Oteiza <oteiza.jor@gmail.com>
2024-02-13 21:09:13 +0000

    lógica lista, definiendo estilos

--- src/js/component/Annnouncement.jsx
+++ src/js/component/Annnouncement.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import "../../styles/App.css";
 import "../../styles/Annnouncement.css";
 
@@ -6,7 +6,7 @@ export default class Annnouncement extends Component {
   render() {
     return (
       <div className={this.props.winner ? "visible" : "hidden"}>
-        <h2>Game Over</h2>
+        <h2>Game Over - {this.props.winner}</h2>
       </div>
     );
   }
